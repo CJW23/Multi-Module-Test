5ccf019f65790cc828fb77dcacb9ba8787f8588e
CJW23 <cjw7242@gmail.com>
2021-12-19 22:08:19 +0900

    feat: 멀티 모듈 작업

---
+++ core/src/main/java/kr/multi/ex/TestService.java
@@ -0,0 +1,10 @@
+package kr.multi.ex;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    public String test() {
+        return "core의 Bean Class 테스트";
+    }
+}
